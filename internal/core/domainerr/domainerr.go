e9df2bb6c0cad3f65a0f2768c60ca108070b4ee3
Askhat Zaripov <askhatzaripov@gmail.com>
2024-01-14 20:25:04 +0300

    feat: core domain error

---
+++ internal/core/domainerr/domainerr.go
@@ -0,0 +1,22 @@
+package domainerr
+
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	rootErr = errors.New("domain defined error")
+)
+
+func New(msg string) error {
+	return fmt.Errorf("%w: %v", rootErr, msg)
+}
+
+func Is(err error) bool {
+	return errors.Is(err, rootErr)
+}
+
+func Join(errs ...error) error {
+	return errors.Join(errs...)
+}
