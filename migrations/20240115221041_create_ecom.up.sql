3d676b743c52243f5a6d644605f5db5f52327241
Askhat Zaripov <askhatzaripov@gmail.com>
2024-01-18 15:19:12 +0300

    fix: use serial type on id instead of bigint

--- migrations/20240115221041_create_ecom.up.sql
+++ migrations/20240115221041_create_ecom.up.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS cart_items (
-  id bigint PRIMARY KEY NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
   cart_id bigint NOT NULL,
   product_id bigint NOT NULL,
   quantity bigint
 );
 
 CREATE TABLE IF NOT EXISTS carts (
-  id bigint PRIMARY KEY NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
   customer_id bigint NOT NULL,
   total_price bigint,
   created_at timestamp,
@@ -14,13 +14,13 @@ CREATE TABLE IF NOT EXISTS carts (
 );
 
 CREATE TABLE IF NOT EXISTS categories (
-  id bigint PRIMARY KEY NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(50) NOT NULL,
   description text
 );
 
 CREATE TABLE IF NOT EXISTS customers (
-  id bigint PRIMARY KEY NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
   user_id bigint NOT NULL,
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50),
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS customers (
 );
 
 CREATE TABLE IF NOT EXISTS order_items (
-  id bigint PRIMARY KEY NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
   order_id bigint NOT NULL,
   product_id bigint NOT NULL,
   quantity bigint,
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS order_items (
 );
 
 CREATE TABLE IF NOT EXISTS orders (
-  id bigint PRIMARY KEY NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
   customer_id bigint NOT NULL,
   status VARCHAR,
   created_at timestamp,
@@ -49,7 +49,7 @@ CREATE TABLE IF NOT EXISTS orders (
 );
 
 CREATE TABLE IF NOT EXISTS products (
-  id bigint PRIMARY KEY NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
   category_id bigint NOT NULL,
   name VARCHAR,
   description text,
@@ -57,20 +57,20 @@ CREATE TABLE IF NOT EXISTS products (
 );
 
 CREATE TABLE IF NOT EXISTS users (
-  id bigint PRIMARY KEY NOT NULL,
-  username VARCHAR(100),
+  id SERIAL PRIMARY KEY NOT NULL,
+  username VARCHAR(100) UNIQUE,
   password VARCHAR(255),
   role VARCHAR(50),
   created_at timestamp,
-  updated_at timestamp
+  updated_at timestamp,
+  last_login_attempt timestamp
 );
 
-
-ALTER TABLE IF NOT EXISTS cart_items ADD FOREIGN KEY (cart_id) REFERENCES carts (id);
-ALTER TABLE IF NOT EXISTS cart_items ADD FOREIGN KEY (product_id) REFERENCES products (id);
-ALTER TABLE IF NOT EXISTS orders ADD FOREIGN KEY (customer_id) REFERENCES customers (id);
-ALTER TABLE IF NOT EXISTS carts ADD FOREIGN KEY (customer_id) REFERENCES customers (id);
-ALTER TABLE IF NOT EXISTS order_items ADD FOREIGN KEY (order_id) REFERENCES orders (id);
-ALTER TABLE IF NOT EXISTS order_items ADD FOREIGN KEY (product_id) REFERENCES products (id);
-ALTER TABLE IF NOT EXISTS products ADD FOREIGN KEY (category_id) REFERENCES categories (id);
-ALTER TABLE IF NOT EXISTS customers ADD FOREIGN KEY (user_id) REFERENCES users (id);
+ALTER TABLE cart_items ADD FOREIGN KEY (cart_id) REFERENCES carts (id);
+ALTER TABLE cart_items ADD FOREIGN KEY (product_id) REFERENCES products (id);
+ALTER TABLE orders ADD FOREIGN KEY (customer_id) REFERENCES customers (id);
+ALTER TABLE carts ADD FOREIGN KEY (customer_id) REFERENCES customers (id);
+ALTER TABLE order_items ADD FOREIGN KEY (order_id) REFERENCES orders (id);
+ALTER TABLE order_items ADD FOREIGN KEY (product_id) REFERENCES products (id);
+ALTER TABLE products ADD FOREIGN KEY (category_id) REFERENCES categories (id);
+ALTER TABLE customers ADD FOREIGN KEY (user_id) REFERENCES users (id);
