ebec54d3f60537af9acbb6381c9375609969c16e
Askhat Zaripov <askhatzaripov@gmail.com>
2024-01-18 15:32:31 +0300

    chore: http server

--- internal/interface/http_rest/http_rest.go
+++ internal/interface/http_rest/http_rest.go
@@ -4,38 +4,62 @@ import (
 	"context"
 	"ecomsvc/internal/core/config"
 	"ecomsvc/internal/interface/http_rest/common"
-	"fmt"
 	"time"
 
-	"go.uber.org/fx"
-
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
-func New(lc fx.Lifecycle, config *config.Config, handlers []common.Handler) {
+// func New(lc fx.Lifecycle, config *config.Config, handlers []common.Handler) {
+// 	f := fiber.New(
+// 		fiber.Config{
+// 			ErrorHandler: common.ErrorHandler,
+// 			ReadTimeout:  time.Second * 3,
+// 		},
+// 	)
+// 	for _, h := range handlers {
+// 		f.Add(h.Method(), h.Pattern(), append(h.Middleware(), h.Handle)...)
+// 	}
+//
+// 	lc.Append(fx.Hook{
+// 		OnStart: func(_ context.Context) error {
+// 			go func() {
+// 				err := f.Listen(fmt.Sprintf("localhost:%d", config.HttpPort))
+// 				if err != nil {
+// 					panic(err)
+// 				}
+// 			}()
+//
+// 			return nil
+// 		},
+// 		OnStop: func(ctx context.Context) error {
+// 			return f.ShutdownWithContext(ctx)
+// 		},
+// 	})
+// }
+
+func New(ctx context.Context, config *config.Config, handlers []common.Handler) {
 	f := fiber.New(
 		fiber.Config{
-			ErrorHandler: common.ErrorHandler,
-			ReadTimeout:  time.Second * 3,
+			ReadTimeout: time.Second * 3,
 		},
 	)
+
+	f.Use(logger.New())
+
 	for _, h := range handlers {
 		f.Add(h.Method(), h.Pattern(), append(h.Middleware(), h.Handle)...)
 	}
 
-	lc.Append(fx.Hook{
-		OnStart: func(_ context.Context) error {
-			go func() {
-				err := f.Listen(fmt.Sprintf("localhost:%d", config.HttpPort))
-				if err != nil {
-					panic(err)
-				}
-			}()
+	go func() {
+		// err := f.Listen(fmt.Sprintf("%s:%d", config.HttpServer.Host, config.HttpServer.Port))
+		if err := f.Listen(":8080"); err != nil {
+			panic(err)
+		}
+	}()
 
-			return nil
-		},
-		OnStop: func(ctx context.Context) error {
-			return f.ShutdownWithContext(ctx)
-		},
-	})
+	<-ctx.Done()
+	log.Info("Gracefully stopping Fiber Server")
+	f.Shutdown()
 }
