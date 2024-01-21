474dd494af520f8ea9d3fa0b0f32008bff7d2749
Askhat Zaripov <askhatzaripov@gmail.com>
2024-01-21 23:20:39 +0300

    feat: order model

--- internal/infrastructure/repository/order/pgsql/model.go
+++ internal/infrastructure/repository/order/pgsql/model.go
@@ -1,12 +1,34 @@
 package pgsql
 
 import (
+	"ecomsvc/internal/domain/order"
 	"time"
 )
 
 type orderRow struct {
-	Id         int
-	CustomerId int
-	Status     string
-	CreatedAt  time.Time
+	ID        int        `db:"id"`
+	PaymentId int        `db:"payment_id"`
+	UserId    int        `db:"user_id"`
+	Status    string     `db:"status"`
+	CreatedAt *time.Time `db:"created_at"`
+}
+
+func (o *orderRow) ToDomain() *order.Order {
+	return &order.Order{
+		ID:        o.ID,
+		PaymentId: o.PaymentId,
+		UserId:    o.UserId,
+		Status:    o.Status,
+		CreatedAt: o.CreatedAt,
+	}
+}
+
+func toDomain(o orderRow) order.Order {
+	return order.Order{
+		ID:        o.ID,
+		PaymentId: o.PaymentId,
+		UserId:    o.UserId,
+		Status:    o.Status,
+		CreatedAt: o.CreatedAt,
+	}
 }
