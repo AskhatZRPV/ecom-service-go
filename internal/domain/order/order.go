6298a318bd0fa576d9434a5ac1be92449c98f640
Askhat Zaripov <askhatzaripov@gmail.com>
2024-01-21 23:05:49 +0300

    feat: order domain

--- internal/domain/order/order.go
+++ internal/domain/order/order.go
@@ -3,8 +3,9 @@ package order
 import "time"
 
 type Order struct {
-	ID         int
-	CustomerId int
-	Status     string
-	CreatedAt  time.Time
+	ID        int
+	PaymentId int
+	UserId    int
+	Status    string
+	CreatedAt *time.Time
 }
