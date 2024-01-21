af1c2787124380a6b9edf39e4be0cfe83715ec5a
Askhat Zaripov <askhatzaripov@gmail.com>
2024-01-21 23:02:10 +0300

    feat: category domain

--- internal/domain/category/category.go
+++ internal/domain/category/category.go
@@ -1,10 +1,7 @@
 package category
 
-import "time"
-
-type OrderItem struct {
-	ID         int
-	CustomerId int
-	Status     string
-	CreatedAt  time.Time
+type Category struct {
+	ID          int
+	Title       string
+	Description string
 }
--- internal/domain/category/reposityory.go
+++ internal/domain/category/reposityory.go
@@ -1,1 +1,11 @@
 package category
+
+import "context"
+
+type Repository interface {
+	Save(ctx context.Context, c *Category) error
+	FindById(ctx context.Context, id int) (*Category, error)
+	GetAll(ctx context.Context) ([]Category, error)
+	UpdateById(ctx context.Context, c *Category) (*Category, error)
+	Delete(ctx context.Context, id int) (*Category, error)
+}
