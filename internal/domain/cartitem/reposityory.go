9e54ddeb0130cfadd4311d3e3fe20ee6371769ed
Askhat Zaripov <askhatzaripov@gmail.com>
2024-01-21 22:53:30 +0300

    feat: cartitem-domain

--- internal/domain/cartitem/reposityory.go
+++ internal/domain/cartitem/reposityory.go
@@ -1,1 +1,10 @@
 package cartitem
+
+import "context"
+
+type Repository interface {
+	FindById(ctx context.Context, id int) (*CartItem, error)
+	FindAllBySessionId(ctx context.Context, id int) ([]CartItem, error)
+	UpdateById(ctx context.Context, c *CartItem) (*CartItem, error)
+	Delete(ctx context.Context, id int) (*CartItem, error)
+}
