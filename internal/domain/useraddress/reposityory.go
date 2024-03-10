d756930b81dfa67f70fa4820de851eae686da8af
Askhat Zaripov <askhatzaripov@gmail.com>
2024-03-10 22:30:47 +0300

    feat: useraddress domain save

--- internal/domain/useraddress/reposityory.go
+++ internal/domain/useraddress/reposityory.go
@@ -3,7 +3,7 @@ package useraddress
 import "context"
 
 type Repository interface {
-	Save(ctx context.Context, u *UserAddress) error
+	Save(ctx context.Context, u *UserAddress) (int, error)
 	FindById(ctx context.Context, id int) (*UserAddress, error)
 	FindByUserId(ctx context.Context, id int) (*UserAddress, error)
 	UpdateById(ctx context.Context, u *UserAddress) (*UserAddress, error)
