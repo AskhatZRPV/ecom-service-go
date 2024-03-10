9cde89742a3b90f7418eb900aec80178257d3a4a
Askhat Zaripov <askhatzaripov@gmail.com>
2024-03-10 22:27:25 +0300

    feat: user domain save method

--- internal/domain/user/repository.go
+++ internal/domain/user/repository.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Repository interface {
-	Save(context.Context, *User) error
+	Save(context.Context, *User) (int, error)
 	FindByUsername(context.Context, string) (*User, error)
 	UpdateLastLoginAttempt(context.Context, ID, time.Time) error
 }
